93b9683473180b8335a5a39f813dce10624c4d1d
hasan erokyar <herokyar@gmail.com>
2015-04-15 23:44:02 -0700

    Update reverse_string_iteratively.py

--- reverse_string_iteratively.py
+++ reverse_string_iteratively.py
@@ -3,6 +3,7 @@
 def reverse(input):
     return ''.join([input[i] for i in range(len(input)-1, -1, -1)])
     
+    #range(len(input)-1, -1, -1) is a nice way to index the string backwords
 
 #test the code
 s = 'this is my string'
